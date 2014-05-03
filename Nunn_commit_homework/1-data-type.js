ee444fac8a471de303c41f209ff594d625ae77ff
Nunn <whktk@qq.com>
2014-05-03 18:06:45 +0800

    1-data

---
+++ Nunn_commit_homework/1-data-type.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {string} param to be judged
+ * @return {string} "null", "undefined", "object", "boolean", etc.
+ *                  CAUTIONS, customized class object will be returned 
+ *                  its class name. eg-> function Person(){}; var obj = new Person();
+ *                  typeOfParam(obj); // "Person"
+ *-------------------------------------------------------------------------------------
+ */
+
+function typeOfParam(param) {
+	return typeof param;
+}
---
+++ 笔记.js
@@ -0,0 +1,7 @@
+/*笔记*/
+git diff 				//列出文件差别
+git add 				//添加文件     git add . 通配所有，提交所有    add之后变成可提交状态，commit才有效
+git commit -m""   		//把add过的文件提交到本地库，
+git status 				//可以确认哪些被提交，哪些不会被提交
+git push   				//推到远程端去
+git pull 				//拉取
