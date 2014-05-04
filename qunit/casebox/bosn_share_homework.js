69324b12f80cac11c8c8e3b850942ad0c3fa3d81
Bosn Ma <huoyong.msb@alibaba-inc.com>
2014-05-04 10:22:22 +0800

    add qunit test cases

---
+++ qunit/casebox/bosn_share_homework.js
@@ -0,0 +1,21 @@
+module('bosn_share_homework');
+
+test("homework 1, data type", function() {
+    var exist = typeof typeOfParam;
+
+    // empty function
+    function T__1 () {
+    }
+
+    ok(exist, 'typeOfParam existance check');
+    if (exist) {
+        equal(typeOfParam(12345), 'number', 'number check');
+        equal(typeOfParam(true), 'boolean', 'boolean check');
+        equal(typeOfParam("test"), 'string', 'string check');
+        equal(typeOfParam({}), 'object', 'object check');
+        equal(typeOfParam(null), 'null', 'null check');
+        equal(typeOfParam(undefined), 'undefined', 'undefined check');
+        var obj = new T__1();
+        equal(typeOfParam(obj), 'T__1', 'customized class check');
+    }
+});
---
+++ qunit/index.js
@@ -0,0 +1,8 @@
+module('env confirm');
+
+var scripts = [
+    '../../Bosn_share_homework/1-data-type.js',
+    'bosn_share_homework.js'
+];
+
+loadScripts(scripts, function() {});
