6b9675a3f1febea8fc9c8427263d43fb4bf213fe
Bosn Ma <huoyong.msb@alibaba-inc.com>
2014-05-07 14:59:51 +0800

    add homework 4 chapter 3

--- JS_definitive_guide_homework/chapter3.js
+++ JS_definitive_guide_homework/chapter3.js
@@ -13,3 +13,22 @@
 function valExists(valName) {
 
 }
+
+
+/**
+ * 写一个函数判断变量是否未定义或值为undefined
+ * 例子：
+ * var a;
+ * var c = 1;
+ * valUndefined("a"); // true
+ * valUndefined("b"); // true
+ * valUndefined("c"); // false
+ *
+ * @valName {string} 需要判断的变量名
+ * @return  {string} 如果变量名存在，返回true，不存在，返回false
+ *
+ */
+
+function valUndefined(valName) {
+
+}
