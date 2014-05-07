f080c947c0f33df8c498be0357d094e570e9e52a
Nunn <whktk@qq.com>
2014-05-07 22:52:36 +0800

    kengdie

--- JS_definitive_guide_homework/chapter3.js
+++ JS_definitive_guide_homework/chapter3.js
@@ -10,16 +10,40 @@
  *
  */
 
-function valExists(valName) {
-	try{
-		eval(valName)
-		return true;		
-	}catch(e){
-		return false;
-	}
+function valExists(valName){
+	var abcdefg;
+    var abc = 300;
+	return eval(valExistsCode(valName));
 }
-
-
+function valExistsCode(valName) {
+	var arr =[],i=0;
+	arr[i++] = "var result;";
+	arr[i++] = "try{";
+	arr[i++] = valName + ";";
+	arr[i++] = "result=true;";
+	arr[i++] = "}catch(e){";
+	arr[i++] = "result = false;";
+	arr[i++] = "}";
+	arr[i++] = "result";
+	return arr.join('');
+}
+function valUndefined(valName){
+	var abcdefg;
+    var abc = 300;
+	return eval(valUndefinedCode(valName));
+}
+function valUndefinedCode(valName) {
+	var arr =[],i=0;
+	arr[i++] = "var result;";
+	arr[i++] = "try{";
+	arr[i++] = valName + ";";
+	arr[i++] = "if("+valName+" == undefined){result=true;}else{result=false;}";
+	arr[i++] = "}catch(e){";
+	arr[i++] = "result = true;";
+	arr[i++] = "}";
+	arr[i++] = "result";
+	return arr.join('');
+}
 
 /**
  * 写一个函数判断变量是否未定义或值为undefined
